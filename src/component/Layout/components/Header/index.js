6095711724894579926bb1df89a6126243f9e6b2
vudinhan2525 <vudinhan000@gmail.com>
2023-07-31 13:25:57 +0700

    nothing

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,14 +1,21 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/image';
 const cx = classNames.bind(styles);
+
 function Header() {
   return (
     <header className={cx('wrapped')}>
       <div className={cx('inner')}>
-        {/* Logo */}
-        {/* Search */}
-        {/* Control */}
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search-box')}>
+          <input type="text" placeholder="Tìm kiếm" spellCheck={false}></input>
+          <button></button>
+          <button className={cx('search-btn')}></button>
+        </div>
+        <div className={cx('action-box')}></div>
       </div>
     </header>
   );
